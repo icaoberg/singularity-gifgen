1b88ae4f2fbf0bb85468e7f4f863594019c98076
Ivan Cao-Berg <icaoberg@andrew.cmu.edu>
2021-07-27 23:38:20 -0400

    Updated repository

--- 1.2.0/build.sh
+++ 1.2.0/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-IMAGE=singularity-gifgen.simg
+IMAGE=singularity-gifgen-1.2.0.sif
 DEFINITION=Singularity
 
 if [ -f $IMAGE ]; then
 	rm -fv $IMAGE
 fi
 
-singularity build --remote $IMAGE $DEFINITION
+sudo singularity build $IMAGE $DEFINITION
